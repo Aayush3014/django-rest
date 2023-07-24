44bb0ad36e08a074fb914d473951f6ae5094b83b
Aayush3014 <ayushak3008@gmail.com>
2023-07-24 20:37:02 +0530

    Movie Data Fetching API

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import MovieData
 
 
-class MovieData(serializers.ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieData
         fields = ['id', 'name', 'duration', 'rating']
